29102babf6e30c423b3c666556a8294c01d86975
Ryan Wade <ryan@Brookes-MacBook-Pro.local>
2020-03-29 18:31:11 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState } from 'react';
+import Counter from './Counter'
+import CounterHooks from './CounterHooks'
+export const ThemeContext = React.createContext()
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [theme, setTheme] = useState('red')
+  return  (
+    <ThemeContext.Provider value ={{ backgroundColor: theme }}>
+    Counter
+    <Counter initialCount={3}/>
+    Counter Hooks
+    <CounterHooks initialCount={2}/>
 
+    <button onClick={() => setTheme(prevTheme => {
+      return prevTheme === 'red' ? 'blue' : 'red'
+    })}>Toggle Theme</button>
+
+    </ThemeContext.Provider>
+  )
+}
 export default App;
---
+++ src/CounterHooks.js
@@ -0,0 +1,32 @@
+import React, { useState, useContext } from 'react'
+import { ThemeContext } from './App'
+
+
+//USING COUNT NUMBER ONLY. NOT USING STATE OBJECT - MORE SIMPLISTIC
+export default function CounterHooks({ initialCount }) {
+    const [count, setCount] = useState(initialCount)
+    const style = useContext(ThemeContext)
+    return (
+    <div>
+    <button style={style} onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+    <span>{count}</span>
+    <button style={style} onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+    </div>
+    )
+}
+
+//USING FULL SATE OBJECT
+// export default function CounterHooks({ initialCount}) {
+//     const [state, setState] = useState({ count: initialCount })
+//     return (
+//     <div>
+//     <button onClick={() => setState(prevState => {
+//         return {count: state.count - 1}
+//     })}>+</button>
+//     <span>{state.count}</span>
+//     <button onClick={() => setState(prevState => {
+//         return {count: state.count + 1}
+//     })}>+</button>
+//     </div>
+//     )
+// }
