7c483d63ef9b2d3eb6c94cd54e8c5b051e0593de
tidalwade <ryantwade@hotmail.com>
2020-04-07 21:00:02 -0500

    connect-edit

--- src/components/RecipeEdit.js
+++ src/components/RecipeEdit.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import RecipeIngredientEdit from './RecipeIngredientEdit'
 
-export default function RecipeEdit() {
+export default function RecipeEdit({ recipe }) {
   return (
     <div className="recipe-edit">
       <div className="recipe-edit__remove-button-container">
@@ -17,6 +17,7 @@ export default function RecipeEdit() {
           type="text"
           name="name"
           id="name"
+          value={recipe.name}
           className="recipe-edit__input" />
         <label
           htmlFor="cookTime"
@@ -27,6 +28,7 @@ export default function RecipeEdit() {
           type="text"
           name="cookTime"
           id="cookTime"
+          value={recipe.cookTime}
           className="recipe-edit__input" />
         <label
           htmlFor="servings"
@@ -38,6 +40,7 @@ export default function RecipeEdit() {
           min="1"
           name="servings"
           id="servings"
+          value={recipe.servings}
           className="recipe-edit__input" />
         <label
           htmlFor="instructions"
@@ -47,7 +50,9 @@ export default function RecipeEdit() {
         <textarea
           name="instructions"
           className="recipe-edit__input"
-          id="instructions"></textarea>
+          id="instructions"> 
+          {recipe.instructions}
+        </textarea>
       </div>
       <br />
       <label className="recipe-edit__label">Ingredients</label>
@@ -55,8 +60,12 @@ export default function RecipeEdit() {
         <div>Name</div>
         <div>Amount</div>
         <div></div>
-        <RecipeIngredientEdit />
-        <RecipeIngredientEdit />
+        {recipe.ingredients.map(ingredient => (
+          <RecipeIngredientEdit
+            key={ingredient.id}
+            ingredient={ingredient}
+          />
+        ))}
       </div>
       <div className="recipe-edit__add-ingredient-btn-container">
         <button className="btn btn--primary">Add Ingredient</button>
